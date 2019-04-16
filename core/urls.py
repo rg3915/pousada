c0d25fdcfece59d3f19e3e49e610614a5e364425
rg3915 <rg3915@yahoo.com.br>
2019-04-15 21:25:17 -0300

    Arrumando as urls

--- core/urls.py
+++ core/urls.py
@@ -1,38 +1,52 @@
-from django.conf.urls import url, include
+from django.urls import path
 from .views import *
 
 
 urlpatterns = [
-    url(r'^$',home, name='core_home'),
+    path('', home, name='core_home'),
 
-    #url pesso onde lista todas pessoas cadastradas
-    #pesso +id + comando mostra determinada pessoa cadastrada e determinado comando, update ou delete
-    url(r'^pesso/$', pesso_view, name='core_pessoas'),
-    url(r'^pesso/(?P<id>\d+)/$', pesso_view, name='core_pessoas'),
-    url(r'^pesso/(?P<id>\d+)/(?P<cmd>\d+)/$', pesso_view, name='core_pessoas'),
+    path('pesso/', pesso_view, name='core_pessoas'),
+    path('pesso/(<int:pk>/', pesso_view, name='core_pessoas'),
+    path('pesso/(<int:pk>/<int:cmd>/', pesso_view, name='core_pessoas'),
 
-    #url quart onde lista todas quartos cadastradas
-    #quart +id + comando mostra determinada quarto cadastrada e determinado comando, update ou delete
-    url(r'^quart/$', quart_view, name='core_quartos'),
-    url(r'^quart/(?P<id>\d+)/$', quart_view, name='core_quartos'),
-    url(r'^quart/(?P<id>\d+)/(?P<cmd>\d+)/$', quart_view, name='core_quartos'),
+    # url quart onde lista todas quartos cadastradas
+    # quart +id + comando mostra determinada quarto cadastrada e determinado
+    # comando, update ou delete
+    path('quart/', quart_view, name='core_quartos'),
+    path('quart/(<int:pk>/', quart_view, name='core_quartos'),
+    path('quart/(<int:pk>/<int:cmd>/', quart_view, name='core_quartos'),
 
-    #url rota onde lista todas rotas cadastradas
-    #rota +id + comando mostra determinada rota cadastrada e determinado comando, update ou delete
-    url(r'^rota/$', rotati_view, name='core_rotativos'),
-    url(r'^rota/(?P<id>\d+)/$', rotati_view, name='core_rotativos'),
-    url(r'^rota/(?P<id>\d+)/(?P<cmd>\d+)/$', rotati_view, name='core_rotativos'),
+    # url rota onde lista todas rotas cadastradas
+    # rota +id + comando mostra determinada rota cadastrada e determinado
+    # comando, update ou delete
+    path('rota/', rotati_view, name='core_rotativos'),
+    path('rota/(<int:pk>/', rotati_view, name='core_rotativos'),
+    path(
+        'rota/(<int:pk>/<int:cmd>/',
+        rotati_view,
+        name='core_rotativos'
+    ),
 
-    #url men onde lista todos mensalista cadastradas
-    #men +id + comando mostra determinada mensalista cadastrada e determinado comando, update ou delete
-    url(r'^men/$', mensalista_view, name='core_mensalista'),
-    url(r'^men/(?P<id>\d+)/$', mensalista_view, name='core_mensalista'),
-    url(r'^men/(?P<id>\d+)/(?P<cmd>\d+)/$', mensalista_view, name='core_mensalista'),
+    # url men onde lista todos mensalista cadastradas
+    # men +id + comando mostra determinada mensalista cadastrada e determinado
+    # comando, update ou delete
+    path('men/', mensalista_view, name='core_mensalista'),
+    path('men/(<int:pk>/', mensalista_view, name='core_mensalista'),
+    path(
+        'men/(<int:pk>/<int:cmd>/',
+        mensalista_view,
+        name='core_mensalista'
+    ),
 
-    #url movmensal onde lista todas movmensal cadastradas
-    #movmensal +id + comando mostra determinada movmensal cadastrada e determinado comando, update ou delete
-    url(r'^movmensal/$', movmensal_view, name='core_movmensalista'),
-    url(r'^movmensal/(?P<id>\d+)/$', movmensal_view, name='core_movmensalista'),
-    url(r'^movmensal/(?P<id>\d+)/(?P<cmd>\d+)/$', movmensal_view, name='core_movmensalista'),
+    # url movmensal onde lista todas movmensal cadastradas
+    # movmensal +id + comando mostra determinada movmensal cadastrada e
+    # determinado comando, update ou delete
+    path('movmensal/', movmensal_view, name='core_movmensalista'),
+    path('movmensal/(<int:pk>/', movmensal_view, name='core_movmensalista'),
+    path(
+        'movmensal/(<int:pk>/<int:cmd>/',
+        movmensal_view,
+        name='core_movmensalista'
+    ),
 
 ]
--- pousada/urls.py
+++ pousada/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import url, include
-from django.urls import path
+from django.urls import include, path
 from django.contrib import admin
-from django.contrib.auth import urls
 
-#urls que transitam de index para outra
+
 urlpatterns = [
-    url(r'home/', include('website.urls')),
-    url(r'sistema/', include('core.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('home/', include('website.urls')),
+    path('sistema/', include('core.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
 ]
