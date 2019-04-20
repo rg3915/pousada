8ce2d1504ee090df88fafb4e953a32be275d3384
rg3915 <rg3915@yahoo.com.br>
2019-04-20 01:24:29 -0300

    Admin do hotel

--- pousada/hotel/admin.py
+++ pousada/hotel/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
-
-# class MovRotativoAdmin(admin.ModelAdmin):
-#     list_display = ('checkin', 'checkout', 'valor_hora', 'pago', 'total', 'horas_total', 'quarto')
-
-#     def quarto(self, obj):
-#         return obj.quarto
-
-# class MovMensalistaAdmin(admin.ModelAdmin):
-#     list_display = ('mensalista', 'dt_pgto')
+from .models import Pessoa, Quarto, Mensalista, MovRotativo
 
 
-# admin.site.register(Padrao)
-# admin.site.register(Quarto)
-# admin.site.register(Pessoa)
-# admin.site.register(Parametros)
-# admin.site.register(Mensalista)
-# admin.site.register(MovMensalista)
-# admin.site.register(MovRotativo,MovRotativoAdmin)
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'cidade', 'telefone')
+    search_fields = ('nome', 'cpf', 'cidade', 'telefone')
+
+
+@admin.register(Quarto)
+class QuartoAdmin(admin.ModelAdmin):
+    list_display = ('padrao', 'numero', 'hospede', 'cor')
+
+
+@admin.register(Mensalista)
+class MensalistaAdmin(admin.ModelAdmin):
+    list_display = ('quarto', 'inicio', 'valor_mes')
+
+
+@admin.register(MovRotativo)
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = ('checkin', 'checkout', 'valor_hora', 'quarto', 'pago')
