988f72186e305d1475d82d8c1c92def0295b4f32
rg3915 <rg3915@yahoo.com.br>
2019-04-20 01:14:06 -0300

    Recuperando o campo de busca

--- pousada/hotel/views.py
+++ pousada/hotel/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, resolve_url
 from .models import Pessoa
@@ -14,6 +15,14 @@ def dashboard(request):
 def pessoas(request):
     pessoas = Pessoa.objects.all()
     form = PessoaForm()
+
+    # Search
+    search = request.GET.get('search')
+    if search:
+        pessoas = pessoas.filter(
+            Q(nome__icontains=search,) |
+            Q(cpf__icontains=search,)
+        )
     data = {
         'pessoas': pessoas,
         'form': form,
